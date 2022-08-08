a9304132aa21a290a3ba7f9690dc6722ddcca79b
Mike Wilkes <mw.wilkes@gmail.com>
2022-08-08 15:31:55 -0700

    add alternative results dialogue

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,6 @@ function divide(number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert("the numbers add up to " + add(number1, number2)+ ".");
-window.alert("the first number minus the second is " + subtract(number1, number2) + ".");
-window.alert("multiplied together you get " + multiply(number1, number2) + ".");
-window.alert("the divided comes to " + divide(number1, number2)+ ".");
+window.alert(number1 + " + " + number2 + " = " + add(number1, number2) + ", " + number1 + " - " + number2 + " = " + subtract(number1, number2) + ", " + number1 + " x " + number2 + " = " + multiply(number1, number2) + ", and " + number1 + " ÷ " + number2 + " = " + divide(number1, number2));
+
+// window.alert("The numbers add up to " + add(number1, number2) + "." + " The first number minus the second is " + subtract(number1, number2) + "." + " Multiplied together you get " + multiply(number1, number2) + "." + " And dividing them makes " + divide(number1, number2) + ".");
