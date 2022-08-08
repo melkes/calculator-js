abb745edab6bf2b2c5f105c220ea75f05568200d
Mike Wilkes <mw.wilkes@gmail.com>
2022-08-08 15:10:21 -0700

    add functions for subtract, multiply, divide, plus text to explain them

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,21 @@
 function add(number1, number2) {
   return number1 + number2;
 }
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+function divide(number1, number2) {
+  return number1 / number2;
+}
 
 // user interface logic 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert(add(number1,number2));
+window.alert("the numbers add up to " + add(number1, number2)+ ".");
+window.alert("the first number minus the second is " + subtract(number1, number2) + ".");
+window.alert("multiplied together you get " + multiply(number1, number2) + ".");
+window.alert("the divided comes to " + divide(number1, number2)+ ".");
